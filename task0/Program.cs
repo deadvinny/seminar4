519d40df52efe37a055a16e305d5e60b7157c9a4
Polina <deadvinny1@gmail.com>
2023-07-23 20:38:29 +0300

    Добавлена задача 24

---
+++ task0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+int GetSum(int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число: ");
+
+
+Console.WriteLine($"Сумма чисел от 1 до {num} равна {GetSum(num)}");
