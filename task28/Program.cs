dfb73e5aebf43bf95ce7983c59d0a50ee8c2ac31
Polina <deadvinny1@gmail.com>
2023-07-23 20:38:51 +0300

    Добавлена задача 28

---
+++ task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int GetSum(int limit)
+{
+    int sum = 1;
+    for (int i = 2; i <= limit; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число: ");
+
+
+Console.WriteLine($"Произведение чисел от 1 до {num} равна {GetSum(num)}");
